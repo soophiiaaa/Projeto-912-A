7da23607d5bc2812615363a957fa134820d8f64d
SOPHIA LACERDA BUARQUE MAGALH�ES SILVA <2023313814@MACEIO.IFAL>
2025-02-27 11:13:02 -0300

    fix: corrigir erros

--- routes/users.js
+++ routes/users.js
@@ -6,14 +6,14 @@ const User = require('../models/User')
 
 router.get('/users', async (req, res) => {
     const users = await User.findAll()
-    res.status(200).json();
+    res.status(200).json({ users }); //retorna os usuários cadastrados no banco
 })
 
 router.post('/users', async (req, res) => {
-   await User.create({
-    username:'Sophia Lacerda',
-    birthday: new Date(1980, 6, 20)
-   })
+    await User.create({
+        username: 'Sophia Lacerda',
+        birthday: new Date(2007, 5, 11)
+    }) //toda entidade precisa de um id para um identificador único
 }); //o uso do async/await é importante para o uso de programação assíncrona
 
 router.delete('/users/:id', (req, res) => {
