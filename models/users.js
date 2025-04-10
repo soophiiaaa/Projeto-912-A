48512e96bc457118c46dbb2cd45e1347406895cd
Sophia com PH <157661814+soophiiaaa@users.noreply.github.com>
2025-04-10 11:12:40 -0300

    update users.js

--- models/users.js
+++ models/users.js
@@ -1,25 +1,22 @@
-const { DataTypes } = require('sequelize')
-const sequelize = require('../database/database')
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
 
-// Definição do modelo User
 const User = sequelize.define('User', {
     id: {
-        type: DataTypes.INTEGER, //DataTypes -> classe do sequelize que contém todos os tipos disponíveis nele
+        type: DataTypes.INTEGER,
         autoIncrement: true,
         primaryKey: true
     },
-
     username: {
         type: DataTypes.STRING,
         allowNull: false
     },
-
     birthday: {
         type: DataTypes.DATE,
         allowNull: false
     }
 }, {
     timestamps: false
-})
+});
 
-module.exports = User
+module.exports = User;
